264f575c5eb19c582f65e2e7171ffd7f44d33954
mepheser <matthias.epheser@indoqa.com>
2016-10-11 11:26:07 +0200

    update README

--- README.md
+++ README.md
@@ -4,3 +4,38 @@ A collection of additional rebass components:
 
   * Div: A simple div component, useful for structuring view code without any plain html tags.
   * List/ListItem: Ordered and unordered lists with default padding/margin settings and configurable listStyleType. 
+
+## Example 
+
+```javascript
+import React from 'react'
+import {Space} from 'rebass'
+import {Div, List, ListItem} from 'indoqa-rebass-components'
+
+const ListExample = () => (
+  <Div>
+    <List>
+      <ListItem> item 1 </ListItem>
+      <ListItem> item 2 </ListItem>
+      <ListItem> item 3 </ListItem>
+      <ListItem> item 4 </ListItem>
+    </List>
+    <Space y={1} />
+    <List itemType="circle">
+      <ListItem> item 1 </ListItem>
+      <ListItem> item 2 </ListItem>
+      <ListItem> item 3 </ListItem>
+      <ListItem> item 4 </ListItem>
+    </List>
+    <Space y={1} />
+    <List ordered>
+      <ListItem> item 1 </ListItem>
+      <ListItem> item 2 </ListItem>
+      <ListItem> item 3 </ListItem>
+      <ListItem> item 4 </ListItem>
+    </List>
+  </Div>
+)
+
+export default ListExample
+```
--- src/playground/components/ListExample.react.js
+++ src/playground/components/ListExample.react.js
@@ -6,32 +6,26 @@ import {Space} from 'rebass'
 
 const ListExample = () => (
   <Div>
-    <Div>
-      <List>
-        <ListItem> item 1 </ListItem>
-        <ListItem> item 2 </ListItem>
-        <ListItem> item 3 </ListItem>
-        <ListItem> item 4 </ListItem>
-      </List>
-    </Div>
+    <List>
+      <ListItem> item 1 </ListItem>
+      <ListItem> item 2 </ListItem>
+      <ListItem> item 3 </ListItem>
+      <ListItem> item 4 </ListItem>
+    </List>
     <Space y={1} />
-    <Div>
-      <List itemType="circle">
-        <ListItem> item 1 </ListItem>
-        <ListItem> item 2 </ListItem>
-        <ListItem> item 3 </ListItem>
-        <ListItem> item 4 </ListItem>
-      </List>
-    </Div>
+    <List itemType="circle">
+      <ListItem> item 1 </ListItem>
+      <ListItem> item 2 </ListItem>
+      <ListItem> item 3 </ListItem>
+      <ListItem> item 4 </ListItem>
+    </List>
     <Space y={1} />
-    <Div>
-      <List ordered>
-        <ListItem> item 1 </ListItem>
-        <ListItem> item 2 </ListItem>
-        <ListItem> item 3 </ListItem>
-        <ListItem> item 4 </ListItem>
-      </List>
-    </Div>
+    <List ordered>
+      <ListItem> item 1 </ListItem>
+      <ListItem> item 2 </ListItem>
+      <ListItem> item 3 </ListItem>
+      <ListItem> item 4 </ListItem>
+    </List>
   </Div>
 )
 
