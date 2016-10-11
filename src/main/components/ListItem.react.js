9fbe180a712f153ae7447aaf9b6d68bb564863d6
mepheser <matthias.epheser@indoqa.com>
2016-10-11 12:33:31 +0200

    add default padding

--- src/main/components/ListItem.react.js
+++ src/main/components/ListItem.react.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import {Base} from 'rebass'
 
+const baseStyle = {
+  margin: '0',
+  padding: '0px 0px 0px 5px'
+}
+
 const ListItem = (props) => (
-  <Base {...props} tagName={'li'} />
+  <Base {...props} tagName={'li'} baseStyle={baseStyle} />
 )
 
 export default ListItem
