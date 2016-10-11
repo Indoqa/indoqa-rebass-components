c2038e9b264242d84a1958cf11c293ec0586edab
mepheser <matthias.epheser@indoqa.com>
2016-10-11 14:16:19 +0200

    docs webpack build

---
+++ indoqa-webpack-docs-options.js
@@ -0,0 +1,9 @@
+module.exports = {
+  appName: 'indoqa-rebass-components-docs',
+  mainJs: './src/playground/index.js',
+  outputPath: './docs',
+  outputPublicPath: '/indoqa-rebass-components',
+  isReactLibrary: false,
+  isPlayground: true,
+  externals: {}
+}
