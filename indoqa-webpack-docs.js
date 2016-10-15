0cd58110e9269b7d173661a51ce259f2aaadda79
mepheser <matthias.epheser@indoqa.com>
2016-10-15 15:14:43 +0200

    fix public assets path in docs

--- indoqa-webpack-docs.js
+++ indoqa-webpack-docs.js
@@ -2,7 +2,7 @@ module.exports = {
   appName: 'indoqa-rebass-components-docs',
   mainJs: './src/playground/index.js',
   outputPath: './docs',
-  outputPublicPath: '/indoqa-rebass-components',
+  outputPublicPath: '/indoqa-rebass-components/',
   createIndexHtml: true,
   isReactLibrary: false,
   externals: {}
