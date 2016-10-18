3862be71b6e8e54de3e35b85e9d89f99e75ded66
mepheser <matthias.epheser@indoqa.com>
2016-10-18 12:48:11 +0200

    expose all components

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,15 @@
+import _Anchor from './components/Anchor.react'
+import _AnchorLink from './components/AnchorLink.react'
 import _Div from './components/Div.react'
+import _FontAwesome from './components/FontAwesome.react'
 import _List from './components/List.react'
 import _ListItem from './components/ListItem.react'
+import _Span from './components/Span.react'
 
+export const Anchor = _Anchor
+export const AnchorLink = _AnchorLink
 export const Div = _Div
 export const List = _List
 export const ListItem = _ListItem
+export const Span = _Span
+export const FontAwesome = _FontAwesome
